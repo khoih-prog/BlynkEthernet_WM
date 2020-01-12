38b609b74398c087271bfe6078fd8ca166df39d0
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2020-01-12 02:28:08 -0500

    Add files via upload

---
+++ examples/W5100_Blynk/W5100_Blynk.ino
@@ -0,0 +1,114 @@
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+
+#include <SPI.h>
+#include <Ethernet.h>
+#include <EthernetWebServer.h>
+
+// Start location in EEPROM to store config data. Default 0
+// Config data Size currently is 128 bytes)
+#define EEPROM_START     256
+
+#define USE_SSL     false
+
+#if USE_SSL
+  // Need ArduinoECCX08 and ArduinoBearSSL libraries
+  // Currently, error not enough memory for UNO, Mega2560. Don't use
+  #include <BlynkSimpleEthernetSSL_WM.h>
+#else
+  #include <BlynkSimpleEthernet_WM.h>
+#endif
+
+#define USE_BLYNK_WM      true
+
+#if !USE_BLYNK_WM
+  #define USE_LOCAL_SERVER      true
+
+  #if USE_LOCAL_SERVER
+    char auth[] = "******";
+    char server[] = "account.duckdns.org";
+    //char server[] = "192.168.2.112";
+  #else
+    char auth[] = "******";
+    char server[] = "blynk-cloud.com";
+  #endif
+  
+  #define BLYNK_HARDWARE_PORT       8080
+#endif
+
+#define W5100_CS  10
+#define SDCARD_CS 4
+
+void setup()
+{
+  // Debug console
+  Serial.begin(115200);
+  Serial.println("\nStart W5100_Blynk");
+
+  pinMode(SDCARD_CS, OUTPUT);
+  digitalWrite(SDCARD_CS, HIGH); // Deselect the SD card
+
+#if USE_BLYNK_WM
+  Blynk.begin();
+#else
+  #if USE_LOCAL_SERVER
+    Blynk.begin(auth, server, BLYNK_HARDWARE_PORT);
+  #else
+    Blynk.begin(auth);
+    // You can also specify server:
+    //Blynk.begin(auth, server, BLYNK_HARDWARE_PORT);
+  #endif
+#endif
+
+  if (Blynk.connected())
+  {
+    Serial.print("Conn2Blynk: server=");
+    Serial.print(Blynk.getServerName());
+    Serial.print(", port=");
+    Serial.println(Blynk.getHWPort());
+    Serial.print("Token=");
+    Serial.print(Blynk.getToken());  
+    Serial.print(", IP=");
+    Serial.println(Ethernet.localIP());
+  }
+}
+
+void heartBeatPrint(void)
+{
+  static int num = 1;
+
+  if (Blynk.connected())
+    Serial.print("B");
+  else
+    Serial.print("F");
+  
+  if (num == 80) 
+  {
+    Serial.println();
+    num = 1;
+  }
+  else if (num++ % 10 == 0) 
+  {
+    Serial.print(" ");
+  }
+} 
+
+void check_status()
+{
+  static unsigned long checkstatus_timeout = 0;
+
+#define STATUS_CHECK_INTERVAL     60000L
+
+  // Send status report every STATUS_REPORT_INTERVAL (60) seconds: we don't need to send updates frequently if there is no status change.
+  if ((millis() > checkstatus_timeout) || (checkstatus_timeout == 0))
+  {
+    heartBeatPrint();
+    checkstatus_timeout = millis() + STATUS_CHECK_INTERVAL;
+  }
+}
+
+void loop()
+{
+  Blynk.run();
+  check_status();
+}
